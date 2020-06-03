2b772c6a859fa02465760b8a935944286f65035d
dsmaldone <38856598+dsmaldone@users.noreply.github.com>
2020-06-04 00:21:34 +0200

    Update vm-auto-delete.sh

--- vm-auto-delete.sh
+++ vm-auto-delete.sh
@@ -19,6 +19,11 @@
 # Released under GNU/GPL3 see here: https://www.gnu.org/licenses/gpl-3.0.en.html
 # Source: https://github.com/dsmaldone/FreePBX/blob/master/vm-auto-delete.sh
 #
+# The scripr accepts 0 or 1 optional cli parameters.
+# Optional Parameter: a comma separated value of all extension's voicemail to be deleted. 
+# If no argument is input, voicemails of all the existing extensions will be deleted.
+# Usage: vm-auto-delete.sh [<ext1,ext2,ext3...>]
+#
 # Adjustable Parameters
 # vm_dir: Voicemail directory root fullpath
 # days: number of days to go back from today to start deleting messages
@@ -28,18 +33,12 @@
 #
 # Important: If the 'find' program it’s not found - Give Up!
 
-#!/bin/sh
-# A Simple Tool to remote Voicemail Messages older than $days
-# vm_dir = Voicemail directory root
-# days = number of days to go back to start deleting messages
-# If find program it's not found - Give Up!
-
 narg=$#
 
-days=30
+days=7
 vm_dir="/var/spool/asterisk/voicemail/"
 
-
+regexp="^([0-9]*(,)?)+$"
 find_loc=$(command -v find)
 ftest=$?
 
@@ -57,16 +56,24 @@ elif [ $narg -eq 0 ]; then
         logger $(date) - Executed Vociemail Cleanup: All voicemail messages older than $days days have been deleted
         $find_loc $vm_dir -name "msg*" -mtime +$days -type f -exec rm -rf {} \;
 else
-        for i in $(echo $1 | sed "s/,/ /g")
-        do
-                curvm=$vm_dir/default/$i
-                if [ -d "$curvm" ]; then
-                        logger $(date) - Checking Extension $i Voicemail
-                        $find_loc $curvm -name "msg*" -mtime +$days -type f -exec rm -rf {} \;
-                        # Uncomment follow line for debug
-                        # echo "checked $i"
-                        vmchecked="${vmchecked}$i,"
-                fi
-        done
-        logger $(date) - Executed Voicemail Cleanup: All $vmchecked messages older than $days days have been deleted
+        if [[ $1 =~ $regexp ]]; then
+                # Uncomment following line for debug
+                # echo "found a match"
+                for i in $(echo $1 | sed "s/,/ /g")
+                do
+                        curvm=$vm_dir/default/$i
+                        if [ -d "$curvm" ]; then
+                                logger $(date) - Checking Extension $i Voicemail
+                                $find_loc $curvm -name "msg*" -mtime +$days -type f -exec rm -rf {} \;
+                                # Uncomment following line for debug
+                                # echo "checked $i"
+                                vmchecked="${vmchecked}$i,"
+                        fi
+                done
+                logger $(date) - Executed Voicemail Cleanup: All $vmchecked messages older than $days days have been deleted
+        else
+                echo "Invalid argument: allowed is comma separated extensions"
+                exit 3
+        fi
 fi
+exit 0
